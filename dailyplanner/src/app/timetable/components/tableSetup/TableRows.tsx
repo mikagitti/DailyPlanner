4e7ee9480e173b3ba4165c49cf235fde921057b3
mikagitti <mikagitti@gmail.com>
2024-09-16 09:09:45 +0300

    - Added functionality to show meeting today and tomorrow in different color. Highlight close meetings. - Changed css for rows layout: Font style and row height.

--- dailyplanner/src/app/timetable/components/tableSetup/TableRows.tsx
+++ dailyplanner/src/app/timetable/components/tableSetup/TableRows.tsx
@@ -11,16 +11,49 @@ export default function TableRows({ meetings, setSelectedMeeting }: TableBuilder
     return (
         <>
             <tbody className="table-rows">
-                {meetings && meetings.map((item, index) => (
-                    <tr className="table-row" key={index} onClick={() => handleRowClick(item)}>
-                        <td> {getWeekday(item.date)} </td>
-                        <td> {formatDate(item.date)} </td>
-                        <td> {getDigitalTime(item.date)} </td>
-                        <td> {item.event} </td>
-                        <td> {item.location} </td>
-                    </tr>
+                {meetings && meetings.map((item) => (
+                    <ShowTableRow key={item.id} item={item} handleRowClick={handleRowClick} />
                 ))}
             </tbody>
         </>
     );
 }
+
+
+const ShowTableRow = ({ item, handleRowClick }: { item: meetingType, handleRowClick: (item: meetingType) => void }) => {
+    return (
+        <tr className={isTomorrow(item.date)} key={item.id} onClick={() => handleRowClick(item)}>
+            <td> {getWeekday(item.date)} </td>
+            <td> {formatDate(item.date)} </td>
+            <td> {getDigitalTime(item.date)} </td>
+            <td> {item.event} </td>
+            <td> {item.location} </td>
+        </tr>
+    )
+}
+
+const isTomorrow = (date: Date) => {
+
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+
+    const meetingDate = new Date(date);
+
+    if (
+        meetingDate.getDate() === today.getDate() &&
+        meetingDate.getMonth() === today.getMonth() &&
+        meetingDate.getFullYear() === today.getFullYear()
+    ) {
+        return 'table-row today';
+    } else if (
+        meetingDate.getDate() === tomorrow.getDate() &&
+        meetingDate.getMonth() === tomorrow.getMonth() &&
+        meetingDate.getFullYear() === tomorrow.getFullYear()
+    ) {
+        return 'table-row tomorrow';
+    }
+    else {
+        return 'table-row';
+    }
+};
