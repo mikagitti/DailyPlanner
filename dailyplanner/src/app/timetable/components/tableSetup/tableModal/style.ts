51806a316b89b231ee8f1660fca6e6b61f4722ed
mikagitti <mikagitti@gmail.com>
2024-09-24 10:30:41 +0300

    - Added sanitization to modal form saving. Prevent XSS. - Removed page.module.css styling. - Modal styling refining.

--- dailyplanner/src/app/timetable/components/tableSetup/tableModal/style.ts
+++ dailyplanner/src/app/timetable/components/tableSetup/tableModal/style.ts
@@ -1,9 +1,10 @@
 export const styles = {
      formGroup: {
           margin: "10px 0",
-          fontSize: "1.4em",
+          fontSize: "1.4rem",
      },
      error: {
           color: "red",
+          fontSize: "1.2rem",
      },
 };
